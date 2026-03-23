d8ff85f2be768e9cb403a413c4c9a4191794d69a
Phantom <bhardwajprashant7045@gmail.com>
2026-03-23 13:48:49 +0530

    initial commit

---
+++ src/test/java/com/cts/eduLinkM/EduLinkMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.eduLinkM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduLinkMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
